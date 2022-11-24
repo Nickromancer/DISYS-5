ff8ec4f71a87e729ae39eb2de9b0dc23d8bb8366
oljh <oljh@itu.dk>
2022-11-24 15:49:17 +0100

    New logs

--- concatenate_logs.sh
+++ concatenate_logs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-echo "# Logs" > log.txt
+echo "Logs (the numbers refers to the ports of the nodes. Refer to the readme.md for explanation.)" > log.txt
 find . -name 'logfile.*' -exec echo -e '\n' >> log.txt \; -exec echo {} >> log.txt \; -exec cat {} >> log.txt \;
